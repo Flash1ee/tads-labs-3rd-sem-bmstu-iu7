ab07120ba81fe4cb96b5c8a705987459a6699967
Dmitry Varin <varindv@gmail.com>
2021-01-28 22:13:39 +0300

    fix repo

--- lab_06/show_graph.sh
+++ lab_06/show_graph.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 dot -Tpng ./data/graph_bst_beg.gv -o./data/graph_bst_beg.png
 dot -Tpng ./data/graph_bst_end.gv -o./data/graph_bst_end.png
-
 dot -Tpng ./data/graph_avl.gv -o./data/graph_avl.png
 
